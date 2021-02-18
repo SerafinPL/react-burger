77698ab81b2ed378060793ecd5e82f203c5e9711
serafinpl <ser.burberibar@gmail.com>
2021-02-18 17:09:08 +0100

    elementy navigacji

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import navigationItem from './NavigationItem/NavigationItem'
+
+const navigationItems = (props) => (
+		<ul>
+			<li><a href='/'>A link</a></li>
+
+		</ul>
+	);
+
+export default navigationItems;
