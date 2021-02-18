0c6ba891f2ae3e45f4bb97605666f2e0ea3dc062
serafinpl <ser.burberibar@gmail.com>
2021-02-18 19:32:42 +0100

    Dodanie SideDraer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,12 +4,14 @@ import classes from './Layout.module.css';
 
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => {
 
 	return(
 	<Aux>
 		<Toolbar/>
+		<SideDrawer/>
 		<main className={classes.content}>
 			{props.children}
 		</main>
