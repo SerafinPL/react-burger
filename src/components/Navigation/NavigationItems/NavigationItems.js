84982e46704753dbc92a362227f6fdd406d2b1b2
serafinpl <ser.burberibar@gmail.com>
2021-02-18 19:23:22 +0100

    Sider drawer added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ import classes from './NavigationItems.module.css';
 
 const navigationItems = (props) => (
 		<ul className={classes.NavigationItems}>
-			<NavigationItem link='/' active{/*to zrobi true*/}>Burger Builder</NavigationItem>
+			<NavigationItem link='/' active>Burger Builder</NavigationItem>
 			<NavigationItem link='/'>Zamówienie</NavigationItem>
 
 		</ul>
