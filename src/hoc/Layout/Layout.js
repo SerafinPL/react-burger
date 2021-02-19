07690d262537cc3e732538e2d19e079839636e1b
serafinpl <ser.burberibar@gmail.com>
2021-02-19 22:04:49 +0100

    make a Aux component in folder Aux

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import classes from './Layout.module.css';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
