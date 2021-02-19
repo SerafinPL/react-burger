9c6a52660a0d793cc9635d0f36ad813344e89720
serafinpl <ser.burberibar@gmail.com>
2021-02-19 21:01:27 +0100

    poprawki

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,7 +3,12 @@ import React from 'react';
 import classes from './DrawerToggle.module.css';
 
 const drawerToggle = (props) => (
-		<div onClick={props.clicked} className={classes.Menu}>Menu</div>
+		<div onClick={props.clicked} className={classes.DrawerToggle}>
+			<div></div>
+			<div></div>
+			<div></div>
+
+		</div>
 	);
 
 export default drawerToggle;
