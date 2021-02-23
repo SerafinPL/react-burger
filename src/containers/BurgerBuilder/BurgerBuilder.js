0e929179261d8fb2b7fae5195d11d5d0ad40eb23
serafinpl <ser.burberibar@gmail.com>
2021-02-23 22:36:38 +0100

    Added post request to the firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
 	salad: 0.5,
 	cheese: 0.4,
@@ -96,7 +98,25 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseContinueHandler = () => {
-			alert('kontynuujesz ale niestety kanapki nie skosztujesz :/');
+			//alert('kontynuujesz ale niestety kanapki nie skosztujesz :/');
+			const order = {
+				ingredients: this.state.ingredients,
+				price: this.state.totalPrice,
+				customer: {
+					name: 'KubaKoder',
+					address:{
+						street: 'zamkowa 3',
+						zipCode: '40555',
+						city: 'K-ce',
+						country: 'Poland'
+					},
+					email: 'testowy@test.pl'
+				},
+				deliveryMethod: 'fastest'
+			}
+			axios.post('/orders.json', order)
+				.then(response => console.log(response))
+				.catch(error => console.log(error));
 	}
 
 	render(){
