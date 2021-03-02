9c213e322eb5aac40d43fff5f1148402e33b777f
serafinpl <ser.burberibar@gmail.com>
2021-03-02 21:32:35 +0100

    contact Data form

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,35 @@
+import React {Component} from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+
+class ContactData extends Component {
+
+	state ={
+		name: '',
+		email: '',
+		address: {
+			street: '',
+			postalCode: '',
+			city: ''
+		}
+	}
+
+	render(){
+
+		return(
+				<div>
+					<h4>Wprowadź dane do zamówienia:</h4>
+					<form>
+						<input type='text' name='name' placeholder='Twoje Imię'/>
+						<input type='text' name='email' placeholder='Twój Email'/>
+						<input type='text' name='street' placeholder='Ulica'/>
+						<input type='text' name='postalCode' placeholder='Kod pocztowy'/>
+						<input type='text' name='city' placeholder='Miasto'/>
+						<Button btnType='Success'>Zamów</Button>
+					</form>
+				</div>
+			);
+	}
+}
+	
+export default ContactData
