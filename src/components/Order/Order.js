79b42109ad267ecedb32d1f5ac49e21bd12d44b3
serafinpl <ser.burberibar@gmail.com>
2021-03-03 15:39:46 +0100

    order structure and styling

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const Order = () => {
+const Order = (props) => {
 
 	return(
-
+			<div className={classes.Order}>
+				<p>Składniki:</p>
+				<p>Cena: <strong>15,00</strong> zł</p>
+			</div>
 		);
 
 }
