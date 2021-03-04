48eb2cf372301faec49239236f4abc6e7906fc0b
serafinpl <ser.burberibar@gmail.com>
2021-03-04 14:27:01 +0100

    form is done for now

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 import classes from './Input.module.css';
 
-const Input = (props) => (
-		<div></div>
-	);
+const Input = (props) => {
+
+	let inputElement = null;
+
+	switch (props.inputType){
+		case('input'):
+			inputElement = <input {...props}/>;
+		break;
+		case('textarea'):
+			inputElement = <textarea {...props}/>;
+		break;
+		default:
+			inputElement = <input {...props}/>;
+	}
+	
+
+		return(
+			<div>
+				<label>{props.label}</label>
+				{inputElement}
+			</div>
+		);
+	};
 
 
 export default Input;
