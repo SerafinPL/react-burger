5cce4c929c11ff0766ecf55ac2399c63738c5df7
serafinpl <ser.burberibar@gmail.com>
2021-03-04 19:00:49 +0100

    select input is confing good for now

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -14,10 +14,24 @@ const Input = (props) => {
 						value={props.value}/>;
 		break;
 		case('textarea'):
-			inputElement = <textar
-						ea className={classes.InputElement} 
+			inputElement = <textarea 
+						className={classes.InputElement} 
 						{...props.elementConfig} 
 						value={props.value}/>;
+		break;
+		case('select'):
+			inputElement = (<select 
+								className={classes.InputElement} 
+								
+								value={props.value}>
+								{props.elementConfig.options.map(option => (
+									<option key={option.value} value={option.value}>
+										{option.displayValue}
+									</option>
+									)
+								)}
+								
+							</select>);
 		break;
 		default:
 			inputElement = <input 
